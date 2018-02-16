2aab93a186bfff159edd87c971d34f52093332bb
Yassin <yassin_taskin@web.de>
2018-02-16 14:51:46 +0100

    Added generator for user model test data and some test for usermode_dao

---
+++ test/model_db_access_layer/user_model_test_data.py
@@ -0,0 +1,71 @@
+
+
+# https://moodle.ikarion-projekt.de/mod/forum/view.php?id={}
+ARTEFACT_TEMPLATE = "https://moodle.test-data.de/mod/{}/view.php?id={}"
+
+
+def fill_user_model(uid, course, active_days, n_artifacts, updated_at = 1416649608):
+    skeleton = create_user_skeleton()
+    skeleton["uid"] = str(uid)
+    skeleton["course"] = str(course)
+    skeleton["updated_at"] = str(updated_at)
+    skeleton["active_days"] = active_days
+
+    artifacts_actions = {"videos": ["viewed"],
+                         "literature": ["viewed"],
+                         "forum_posts": ["viewed", "created"],
+                         "quizzes": ["taken"],
+                         "wiki_articles": ["viewed", "updated"]}
+
+    for artifact_type, action_types in artifacts_actions.items():
+        for action_type in action_types:
+            for n in range(n_artifacts):
+                action_id = ARTEFACT_TEMPLATE.format(artifact_type, n)
+                action_data = create_action(action_id)
+                add_action(skeleton, artifact_type, action_type, action_data)
+
+    return skeleton
+
+
+def create_user_skeleton():
+    skeleton = {
+        "uid": "user_id",
+        "course": "course_id",
+        "updated_at": "last_update_timestamp",
+        "active_days": [1, 2],
+        "artifacts": {
+            "videos": {
+                "viewed": []
+            },
+            "literature": {
+                "viewed": []
+            },
+            "forum_posts": {
+                "viewed": [],
+                "created": []
+            },
+            "quizzes": {
+                "taken": []
+            },
+            "wiki_articles": {
+                "viewed": [],
+                "updated": []
+            }
+        }
+    }
+    return skeleton
+
+
+def add_action(user_model, artifact_type, action, action_data):
+    user_model["artifacts"][artifact_type][action] = action_data
+
+
+def create_action(action_id, **kwargs):
+    action_data = {
+        "id": action_id
+    }
+    return {**action_data, **kwargs}
+
+
+
+
