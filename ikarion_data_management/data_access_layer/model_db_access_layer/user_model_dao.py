7dd6d1346cdb21aea9313850b5edd5f0e24b8960
Yassin <yassin_taskin@web.de>
2018-02-16 14:50:55 +0100

    Fixed bug with user_model_dao

--- ikarion_data_management/data_access_layer/model_db_access_layer/user_model_dao.py
+++ ikarion_data_management/data_access_layer/model_db_access_layer/user_model_dao.py
@@ -5,6 +5,8 @@ RES_USED_LIST = 'ressources_list'
 RES_USED_FIELD = 'resource_accesses'
 DISTINCT_RES_USED = 'distinct_resource_accesses'
 DISTINCT_RES_USED = 'distinct_resource_accesses'
+
+
 # ...
 
 # Retrieve
@@ -13,18 +15,18 @@ def getAllCourses():
 
 
 def getAllUsers():
-    return con.db.usermodels.distinct('user')
+    return con.db.usermodels.distinct('uid')
 
 
 def getAllUsersForCourse(course):
-    docs = con.db.usermodels.find({'course':course}, {'user': 1})
+    docs = con.db.usermodels.find({'course': course}, {'uid': 1})
     if docs.count == 0:
         raise NoSuchCourseException(course)
     return list(docs)
 
 
 def getAllCoursesForUser(user):
-    docs = con.db.usermodels.find({'user': user}, {'course': 1})
+    docs = con.db.usermodels.find({'uid': user}, {'course': 1})
     if docs.count() == 0:
         raise NoSuchUserException(user, "all courses")
 
@@ -32,9 +34,10 @@ def getAllCoursesForUser(user):
 
 
 def getUserModel(user, course):
-    doc = con.db.usermodels.find_one({'user':user, 'course':course}, {'_id':0})
+    doc = con.db.usermodels.find_one({'uid': user, 'course': course}, {'_id': 0})
     if doc is None:
         raise NoSuchUserException(user, course)
+    return doc
 
 
 def getUserModelsForCourse(course):
@@ -46,16 +49,16 @@ def getUserModelsForCourse(course):
 
 # Update
 def updateOrInsertUserModel(user, course, newModel):
-     con.db.usermodels.updateOne(
-         {'user': user, 'course': course},
-         {'$set': newModel},
-         upsert=True
-     )
+    con.db.usermodels.updateOne(
+        {'uid': user, 'course': course},
+        {'$set': newModel},
+        upsert=True
+    )
 
 
 def updateUserModelSetField(user, course, field, incValue):
     res = con.db.usermodels.updateOne(
-        {'user': user, 'course': course},
+        {'uid': user, 'course': course},
         {'$set': {field: incValue}}
     )
 
@@ -65,7 +68,7 @@ def updateUserModelSetField(user, course, field, incValue):
 
 def updateUserModelIncrementField(user, course, field, incValue):
     res = con.db.usermodels.updateOne(
-        {'user': user, 'course': course},
+        {'uid': user, 'course': course},
         {'$inc': {field: incValue}}
     )
 
@@ -75,12 +78,10 @@ def updateUserModelIncrementField(user, course, field, incValue):
 
 # Exception classes
 class NoSuchUserException(Exception):
-
     def __init__(self, user, course):
         self.message = "No such user {} in course {}".format(user, course)
 
 
 class NoSuchCourseException(Exception):
-
     def __init__(self, course):
         self.message = "No such course {}".format(course)
