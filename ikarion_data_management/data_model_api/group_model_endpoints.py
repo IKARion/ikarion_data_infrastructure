a5bd37c54fecc06e5f946a5fd364225f49210b82
doberstein <dorian.doberstein@unitybox.de>
2018-02-28 15:13:14 +0100

    added necessary functions implemented: getLatencies

--- ikarion_data_management/data_model_api/group_model_endpoints.py
+++ ikarion_data_management/data_model_api/group_model_endpoints.py
@@ -3,6 +3,52 @@ from ..data_access_layer.model_db_access_layer import group_model_dao
 
 group_model_endpoints = Blueprint('group_model_endpoints', __name__)
 
+# models: group content, group sequence, group structure
+
 @group_model_endpoints.route('/about')
 def about():
     return 'Group model endpoints.'
+
+@group_model_endpoints.route("/")
+def getAllGroups():
+    return jsonify(model=group_model_dao.getGroups())
+
+def getGroupsInContext(context):
+    # context: task or course or both
+    return 'todo'
+
+def getContexts():
+    # context: zeitraum, kurs, resourcen, name
+    return 'todo'
+
+@group_model_endpoints.route('/models')
+def models():
+    return 'Group models'
+
+@group_model_endpoints.route('/models/<string:course>/<string:group>/<string:context>')
+def getLatencies(course, group, context):
+    latency, latencies = group_model_dao.getLatencies(course, group, context)
+    return jsonify(average_latency=latency, latencies=latencies)
+
+def getAllLatencies(context):
+    return 'todo'
+
+def getConceptCoverage(group, context):
+    return 'todo'
+
+def getActionsOfUsers(group, context):
+    return 'todo'
+
+def getProducedContentByUsers(group, context):
+    return 'todo'
+
+def getInactiveUsers(group, context):
+    # needs log for group and groupmembers (log by group formation plugin?)
+    return 'todo'
+
+def getConceptMap(group, context):
+    return 'todo'
+
+# define URL
+# define result for URL call (wiki)
+
