851f36a3e151eedaf51f190f2be9891c65e289d1
hecking <hecking@collide.info>
2018-08-04 01:24:14 +0200

    Now the URL of the course in the context.grouping array is taken as course identifier. This solves the problem that multiple courses with the same id can exist in different Moodle instances that post log data into the database.

--- ikarion_data_management/data_access_layer/model_db_access_layer/user_model_dao.py
+++ ikarion_data_management/data_access_layer/model_db_access_layer/user_model_dao.py
@@ -31,17 +31,43 @@ verb_schema = "verb.id"
 
 # Standart queries
 course_list_query = [
-                        {"$match" : {"object.definition.type":"http://lrs.learninglocker.net/define/type/moodle/course"}},
-                        {"$group" : {"_id" : {"courseid" : "$context.extensions.courseid", "name" : "$object.definition.name"}}},
-                        {"$project" : {"_id" : 0, "courseid" : {"$arrayElemAt" : ["$_id.courseid", 0]}, "name" :  "$_id.name"}}
-                   ]
+    {"$match" : {
+        "context.contextActivities.grouping" : {
+            "$elemMatch" : {"definition.type" : "http://lrs.learninglocker.net/define/type/moodle/course"}
+         }
+     }},
+     {"$project" : {
+         "course" : {
+            "$filter" : {
+                "input" : "$context.contextActivities.grouping",
+                "as" : "item",
+                "cond" : {"$eq": ["$$item.definition.type", "http://lrs.learninglocker.net/define/type/moodle/course"]}
+            }
+         }
+     }},
+     {"$unwind" : "$course"},
+     {"$unwind" : "$course.definition.extensions"},
+     {"$group" : {
+         "_id" : {"courseid" : "$course.definition.extensions.url", "name" : "$course.definition.extensions.fullname"}
+     }},
+     {"$project" : {
+         "_id" : 0, "courseid" : "$_id.courseid", "name" :  "$_id.name"
+     }}
+]
 
 def course_query(course):
     if course is None:
         return {}
     else:
         query = {
-            course_schema: course
+            "context.contextActivities.grouping" : {
+                "$elemMatch" : {
+                    "definition.type" : "http://lrs.learninglocker.net/define/type/moodle/course",
+                    "definition.extensions" : {
+                        "$elemMatch" : {"url" : course}
+                    }
+                }
+            }
         }
         return query
 
