8f40197fda7e60341c8fb18345622f193879519b
doberstein <doberstein@unitybox.de>
2018-08-17 15:39:32 +0200

    added endpoint for git users, repositories and activities

--- ikarion_data_management/data_model_api/user_model_endpoints.py
+++ ikarion_data_management/data_model_api/user_model_endpoints.py
@@ -11,7 +11,7 @@ def about():
 
 
 # User models
-@user_model_blueprint.route("/model/<course>/<user>")
+@user_model_blueprint.route("/model/<path:course>/<user>")
 def get_user_model(course, user):
     return jsonify(data=umd.get_user_model_for_course(user, course))
 
@@ -37,6 +37,22 @@ def get_all_users():
 
 @user_model_blueprint.route("/<course>")
 def get_all_users_for_course(course):
+    print("***5")
+    # print(request.url)
+    # print(request.query_string)
+    # print("***6")
+    print(course)
+    # print("***7")
+    # print(course)
+    # print(request.query_string)
+    print("***8")
+    s = request.query_string
+    print(course)
+    #print(course + "?" + s.decode("utf-8"))
+    #course = course + "?" + s.decode("utf-8")
+
+    course = fix_url_chars(course)
+
     return jsonify(data=umd.get_all_users_for_course(course))
 
 
@@ -45,6 +61,64 @@ def get_all_users_for_course(course):
 def get_all_courses():
     return jsonify(data=umd.get_all_courses())
 
+# test repo2
+@user_model_blueprint.route("/git_users/<repo>")
+def test_for_repo2(repo):
+    print(repo)
+    repo = fix_url_chars(repo)
+    print(repo)
+    return jsonify(data=umd.get_all_users_for_git_repo(repo))
+
+
+# List of repositories
+@user_model_blueprint.route("/repositories")
+def get_all_repositories():
+    return jsonify(data=umd.get_all_group_repos())
+
+# List of activities for repository
+
+@user_model_blueprint.route("/repo_activities/<repo>")
+def get_all_repo_activities(repo):
+    #return "null"
+    #return jsonify(data=umd.get_all_group_repos())
+
+    print("***repo activities***")
+    print(repo)
+    #print(group)
+    # s = request.query_string
+    # print(s)
+    # print("***1")
+    # print(s[:4])
+    # print(s[5:6])
+    # print(s.split('/'))
+    # data = s.decode("utf-8")
+    # data2 = data.split('/')
+    # print(data)
+    # print("query_string")
+    # print(data2)
+    # print("course")
+    # here
+    # course = course + '/' + group + '?' + data2[0]
+    repo = fix_url_chars(repo)
+
+    print("*after*")
+    print(repo)
+    # print(data2[1])
+    # group = data2[1]
+    start_time = 0
+    constraints = []
+    if request.is_json:
+        r_json = request.get_json()
+        if "start_time" in r_json:
+            start_time = float(r_json["start_time"])
+        if "artefact_id" in r_json:
+            artefact_id = r_json["artefact_id"]
+            artefact_constraint = umd.artefact_query(artefact_id)
+            constraints.append(artefact_constraint)
+    group_activities = umd.get_repo_activities(repo, start_time, *constraints)
+
+    return jsonify(data=group_activities)
+
 
 @user_model_blueprint.route("/times/<user>/<course>")
 def get_all_user_times(user, course):
@@ -52,10 +126,18 @@ def get_all_user_times(user, course):
 
 @user_model_blueprint.route("/active_days/<user>/<course>")
 def get_user_active_days(user, course):
+    print("***active_days***")
+    print(user)
+    print(course)
+    s = request.query_string
+    print(s)
+    #course = course + "?" + s.decode("utf-8")
+    course = fix_url_chars(course)
     return jsonify(data=umd.get_user_active_days(user, course))
 
 @user_model_blueprint.route("/avg_latency/<user>/<course>")
 def get_avg_latency(user, course):
+    course = fix_url_chars(course)
     if request.is_json:
         constraints = request.get_json()
     else:
@@ -74,6 +156,31 @@ def get_group_activities(course, group):
     :return:
     :rtype:
     """
+
+    print("***group activities***")
+    print(course)
+    print(group)
+    #s = request.query_string
+    #print(s)
+    #print("***1")
+    #print(s[:4])
+    #print(s[5:6])
+    #print(s.split('/'))
+    #data = s.decode("utf-8")
+    #data2 = data.split('/')
+    #print(data)
+    #print("query_string")
+    #print(data2)
+    #print("course")
+    # here
+    #course = course + '/' + group + '?' + data2[0]
+    course = fix_url_chars(course)
+
+
+    print("*after*")
+    print(course)
+    #print(data2[1])
+    #group = data2[1]
     start_time = 0
     constraints = []
     if request.is_json:
@@ -91,17 +198,24 @@ def get_group_activities(course, group):
 
 @user_model_blueprint.route("/avg_group_latency/<course>/<group>/<startpoint>")
 def get_average_latency_for_group(course, group, startpoint):
+    course = fix_url_chars(course)
 
     return jsonify(data=umd.get_group_average_latency(int(startpoint), group, course))
 
 @user_model_blueprint.route("/groups_for_course/<course>")
 def get_all_groups_for_course(course):
-    print("groups_for_course")
+    print("groups_for_course***")
+    course = fix_url_chars(course)
     return jsonify(data=umd.get_all_groups_for_course(course))
 
 @user_model_blueprint.route("/courses/<user>")
 def get_all_courses_for_user(user):
 
     return jsonify(data=umd.get_all_courses_for_user(user))
+
+def fix_url_chars(string):
+    string = string.replace("$slash$", "/")
+    string = string.replace("$qmark$", "?")
+    return(string)
 
 
